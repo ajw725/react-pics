3466206658acad81ffc4f896b13a988a745eb228
Andrew Weinstein <andrew@altvia.com>
2020-08-16 14:45:31 -0600

    Grid styling

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,10 +1,28 @@
 import React from 'react';
 
 export default class ImageCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.imageRef = React.createRef();
+
+    this.state = { spans: 0 };
+  }
+
+  componentDidMount() {
+    this.imageRef.current.addEventListener('load', this.setSpans);
+  }
+
+  setSpans = () => {
+    const height = this.imageRef.current.clientHeight;
+    const spans = Math.ceil(height / 10);
+    this.setState({ spans });
+  };
+
   render() {
     return (
-      <div>
+      <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
         <img
+          ref={this.imageRef}
           alt={this.props.description}
           src={this.props.url}
         />
